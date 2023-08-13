52b504f76880ada2a4ce0c63f2f8e1b15ed66504
Wojciech Chybowski <mgrtutka@gmail.com>
2023-08-13 08:54:55 +0200

    Change numpy version to 1.25.2 in requirements.txt

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = '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'
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
         animal = request.form["animal"]
+        print(generate_prompt(animal))
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt(animal),
